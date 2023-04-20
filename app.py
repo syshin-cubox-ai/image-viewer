56cab326d9d17a11e3f8e12b416dd011771fd84a
신석용 <syshin@cubox.ai>
2023-04-20 17:05:07 +0900

    final release

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template
 import base64
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def blank():
-    return '파라미터가 없습니다.'
+def view_img():
+    img_url = request.args.get('img_url')
+    if img_url is None:
+        return '<h1>img_url 파라미터를 전달해주세요.</h1>'
+    else:
+        img_url = base64.urlsafe_b64decode(img_url.encode()).decode()
+        return render_template('view.html', img_url=img_url)
 
 
-@app.route('/<img_link>')
-def view_img(img_link):
-    img_link = base64.urlsafe_b64decode(img_link.encode()).decode()
-    return render_template('view.html', img_link=img_link)
+@app.route('/debug')
+def debug():
+    return render_template('debug.html')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+    app.run('0.0.0.0')
